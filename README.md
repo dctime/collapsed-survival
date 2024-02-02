ba9f97f54bb9475bfe9fdc6874f91ad2df45ff78
dctime <daniel.chu916@gmail.com>
2024-02-02 19:13:14 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CollapsedSurvival
+
+Developed with Unreal Engine 5
---
+++ Source/CollapsedSurvival/CollapsedSurvivalGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CollapsedSurvivalGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COLLAPSEDSURVIVAL_API ACollapsedSurvivalGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
