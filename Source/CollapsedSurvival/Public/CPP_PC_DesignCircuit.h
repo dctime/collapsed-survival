c51fec70d31927be8c7595271b38792153daf814
dctime <daniel.chu916@gmail.com>
2024-02-16 19:45:51 +0800

    Added New Cpp File For PC DesignCircuit

---
+++ Source/CollapsedSurvival/Public/CPP_PC_DesignCircuit.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "CPP_PC_DesignCircuit.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COLLAPSEDSURVIVAL_API ACPP_PC_DesignCircuit : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
