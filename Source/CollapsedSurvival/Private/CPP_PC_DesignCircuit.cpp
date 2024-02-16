aa950f70f2caa614c1644b1a51a0253b8a9020e7
dctime <daniel.chu916@gmail.com>
2024-02-16 19:46:20 +0800

    Something Missing

--- Source/CollapsedSurvival/Private/CPP_PC_DesignCircuit.cpp
+++ Source/CollapsedSurvival/Private/CPP_PC_DesignCircuit.cpp
@@ -3,3 +3,49 @@
 
 #include "CPP_PC_DesignCircuit.h"
 
+void ACPP_PC_DesignCircuit::PreviewComponentOnTheBoard(UClass* ComponentClass, UClass* BoardClass)
+{
+	FVector StartTrace;
+	FVector EndTrace;
+	FVector DirTrace;
+
+	DeprojectMousePositionToWorld(StartTrace, DirTrace);
+	EndTrace = StartTrace + DirTrace * TraceLength;
+
+	FHitResult TraceResult;
+	TArray<AActor*> IgnoreActors;
+
+	if (IsValid(PreviewedActor))
+	{
+		IgnoreActors.Add(PreviewedActor);
+	}
+	UKismetSystemLibrary::LineTraceSingle(this, StartTrace, EndTrace, ETraceTypeQuery::TraceTypeQuery1, false, IgnoreActors, EDrawDebugTrace::None, TraceResult, true);
+
+	AActor* HitActor = TraceResult.GetActor();
+	FVector HitLocation = TraceResult.ImpactPoint;
+
+	if (IsValid(PreviewedActor) && PreviewedActor->GetActorLocation() != HitLocation)
+	{
+		PreviewedActor->Destroy();
+		PreviewedActor = nullptr;
+	}
+	
+	if (UGameplayStatics::ObjectIsA(HitActor, BoardClass))
+	{
+		UWorld* World = GetWorld();
+		
+
+		if (PreviewedActor == nullptr)
+		{
+			PreviewedActor = World->SpawnActor<AActor>(ComponentClass, HitLocation, FRotator(0, 0, 0));
+			TArray<AActor*> Overlapping;
+			PreviewedActor->GetOverlappingActors(Overlapping);
+			if (Overlapping.Num() > 0)
+			{
+				PreviewedActor->Destroy();
+				PreviewedActor = nullptr;
+			}
+		}
+	}
+
+}
--- Source/CollapsedSurvival/Public/CPP_PC_DesignCircuit.h
+++ Source/CollapsedSurvival/Public/CPP_PC_DesignCircuit.h
@@ -4,6 +4,8 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
+#include "Kismet/GameplayStatics.h"
+#include "Kismet/KismetSystemLibrary.h"
 #include "CPP_PC_DesignCircuit.generated.h"
 
 /**
@@ -13,5 +15,16 @@ UCLASS()
 class COLLAPSEDSURVIVAL_API ACPP_PC_DesignCircuit : public APlayerController
 {
 	GENERATED_BODY()
+
+protected:
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	AActor* PreviewedActor;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	float TraceLength = 10000.0;
+
+public:
+	UFUNCTION(BlueprintCallable)
+	void PreviewComponentOnTheBoard(UClass* ComponentClass, UClass* BoardClass);
 	
 };
